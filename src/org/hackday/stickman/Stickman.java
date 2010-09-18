023e94f9b4a454cc8269bc38bbb014aee66ab51c
ildar.karimov <ildar.karimov@4ac92a56-eb22-1d44-8f2e-0e4588b0fb0c>
2010-09-18 16:33:11 +0000

    scalable model

--- src/org/hackday/stickman/Stickman.java
+++ src/org/hackday/stickman/Stickman.java
@@ -3,80 +3,81 @@ package org.hackday.stickman;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-
-import android.util.Log;
 
 public class Stickman implements Cloneable {
-	
+
+    private static final int RESCALE_MULT = 1000;
+    public static final int RELATIVE_WIDTH = (int) (0.75f* RESCALE_MULT);
+    public static final int RELATIVE_HEIGHT = (int) (1.0f* RESCALE_MULT);
+
 	protected static int OBJ_ID = 0;
-	
+
 	private static float KOEFF = 1.1f;
-	
-	private static int ST_LEN_VERY_SHORT = (int) (20 * KOEFF);
-	private static int ST_LEN_SHORT = (int) (37 * KOEFF);
-	private static int ST_LEN_AVER = (int) (ST_LEN_SHORT * 1.5 * KOEFF);
-	private static int ST_LEN_LONG = (int) (ST_LEN_AVER * 1.5 * KOEFF);
-	
-	
+
+    private static int ST_LEN_VERY_SHORT = (int) (RESCALE_MULT / 20 * KOEFF);
+    private static int ST_LEN_SHORT = (int) (RESCALE_MULT / 10);
+    private static int ST_LEN_AVER = (int) (ST_LEN_SHORT * 1.5);
+    private static int ST_LEN_LONG = (int) (ST_LEN_AVER * 1.5);
+
+
 	class Edge {
 		public Point mStart;
 		public Point mEnd;
 		public int length;
-		
+
 		public Edge(Point start, Point end, int length) {
 			mStart = start;
 			mEnd = end;
 			this.length = length;
 		}
-		
+
 		public double getLength() {
 			return Math.hypot(mStart.x - mEnd.x, mStart.y - mEnd.y);
 		}
 	}
-	
+
 	class Point extends android.graphics.Point {
-		
+
 		Point(int x, int y) {
 			super(x, y);
 		}
-		
+
 		Point() {
 		}
-		
+
 		@Override
 		public String toString() {
 			return "Point["+x+","+y+"]";
 		}
-		
+
 		protected int id = OBJ_ID++;
 		public boolean mSelected = false;
 		public boolean mBig = false;
 		protected String mBasePoint;
 		protected HashSet<String> mDerivedPoints = new HashSet<String>();
-		
+
 		@Override
 		public int hashCode() {
-			return id; 
+			return id;
 		}
-		
+
 		public void set(Point psource) {
 			x = psource.x;
 			y = psource.y;
 			mSelected = psource.mSelected;
 			mBig = psource.mBig;
 		}
-	} 
-	
+	}
+
 	private HashMap<String, Point> mPoints = new HashMap<String, Point>();
-	
+
 	private ArrayList<Edge> mEdges = new ArrayList<Edge>();
-	private Point mCenterPoint = new Point(ScreenProps.screenWidth/2, ScreenProps.screenHeight/3);
-	
+    private Point mCenterPoint = new Point(RELATIVE_WIDTH / 2, RELATIVE_HEIGHT / 3);
+
 	protected Point cent = new Point(mCenterPoint.x, mCenterPoint.y);
 	protected Point head = new Point();
 	protected Point lhand = new Point();
-	protected Point rhand = new Point(); 
+	protected Point rhand = new Point();
 	protected Point pah = new Point();
 	protected Point lleg = new Point();
 	protected Point rleg = new Point();
@@ -84,9 +85,9 @@ public class Stickman implements Cloneable {
 	protected Point rbothand = new Point();
 	protected Point lbotleg = new Point();
 	protected Point rbotleg = new Point();
-	
+
 	public Stickman() {
-		
+
 		mPoints.put("head", head);
 		mPoints.put("cent", cent);
 		mPoints.put("lhand", lhand);
@@ -98,8 +99,8 @@ public class Stickman implements Cloneable {
 		mPoints.put("rleg", rleg);
 		mPoints.put("lbotleg", lbotleg);
 		mPoints.put("rbotleg", rbotleg);
-		
-		//edges 
+
+		//edges
 		mEdges.add(new Edge(head, cent, ST_LEN_VERY_SHORT));
 		mEdges.add(new Edge(cent, lhand, ST_LEN_SHORT));
 		mEdges.add(new Edge(cent, rhand, ST_LEN_SHORT));
@@ -110,7 +111,7 @@ public class Stickman implements Cloneable {
 		mEdges.add(new Edge(pah, rleg, ST_LEN_AVER));
 		mEdges.add(new Edge(lleg, lbotleg, ST_LEN_AVER));
 		mEdges.add(new Edge(rleg, rbotleg, ST_LEN_AVER));
-		
+
 		rleg.mBasePoint = "pah";
 		lleg.mBasePoint = "pah";
 		pah.mBasePoint = "cent";
@@ -121,13 +122,13 @@ public class Stickman implements Cloneable {
 		rbothand.mBasePoint = "rhand";
 		lbotleg.mBasePoint = "lleg";
 		rbotleg.mBasePoint = "rleg";
-		
+
 		setBig(head);
-		
+
 		setAngle(lhand, Math.PI);
 		setAngle(rhand, 0);
 		setAngle(pah, Math.PI/2);
-		
+
 		setAngle(head, -Math.PI/2);
 		setAngle(lleg, Math.PI/4);
 		setAngle(rleg, Math.PI*3/4);
@@ -136,53 +137,53 @@ public class Stickman implements Cloneable {
 		setAngle(lbotleg, Math.PI*1/4);
 		setAngle(rbotleg, Math.PI*3/4);
 	}
-	
+
 	public ArrayList<Edge> getEdges() {
 		return mEdges;
 	}
-	
+
 	public HashMap<String, Point> getPoints() {
 		return mPoints;
 	}
-	
+
 	public Point getHead() {
 		return mCenterPoint;
 	}
-	
+
 	public void selectPoint(Point p) {
 		for (Point ip : getPoints().values()) {
 			ip.mSelected = false;
 		}
-		
+
 		p.mSelected = true;
 	}
-	
+
 	public void setBig(Point p) {
 		for (Point ip : getPoints().values()) {
 			ip.mBig = false;
 		}
-		
+
 		p.mBig = true;
 	}
-	
+
 	private Point getPoint(String name) {
 		return getPoints().get(name);
 	}
-	
+
 	private double getAngle(int x1, int y1, int x2, int y2) {
 		int dx = x2 - x1;
 		int dy = y2 - y1;
-		double lenght = (int) Math.hypot(dx, dy); 
-		
+		double lenght = (int) Math.hypot(dx, dy);
+
 		double alpha = Math.asin( dy / lenght );
     	if (dx > 0) {
- 
+
     	} else {
     		alpha = Math.PI - alpha;
     	}
     	return alpha;
 	}
-	
+
 	public void setAngle(Point p, int x, int y) {
 		if (p.mBasePoint == null) {
 			return;
@@ -190,7 +191,7 @@ public class Stickman implements Cloneable {
 		double alpha = getAngle(getPoint(p.mBasePoint).x, getPoint(p.mBasePoint).y, x, y);
 		setAngle(p, alpha);
 	}
-	
+
 	public void move(Point p, int x, int y, int dx, int dy) {
 		if (p == cent) {
 //			Log.d("!!!", dx + " "+dy);
@@ -202,18 +203,18 @@ public class Stickman implements Cloneable {
 			setAngle(p, x, y);
 		}
 	}
-	
+
 	private Edge findEdge(Point p1, Point p2) {
 		for (Edge ed : getEdges()) {
-			if ((ed.mEnd == p1 && ed.mStart == p2) 
+			if ((ed.mEnd == p1 && ed.mStart == p2)
 					|| (ed.mEnd == p2 && ed.mStart == p1)) {
 				return ed;
 			}
 		}
 		return null;
 	}
-	
-	public void setAngle(Point p, double alpha) { 
+
+	public void setAngle(Point p, double alpha) {
 		Edge ed = findEdge(p, getPoint(p.mBasePoint));
 		if (ed == null) {
 			return;
@@ -223,7 +224,7 @@ public class Stickman implements Cloneable {
 		int b = (int) (Math.cos(alpha) * len);
 		p.set(getPoint(p.mBasePoint).x + b, getPoint(p.mBasePoint).y + a);
 	}
-	
+
 	public void set(Stickman another) {
 		for (String pname : another.getPoints().keySet()) {
 			getPoint(pname).set( another.getPoint(pname) );
--- src/org/hackday/stickman/StickmanView.java
+++ src/org/hackday/stickman/StickmanView.java
@@ -1,39 +1,37 @@
 package org.hackday.stickman;
 
-import org.hackday.stickman.Stickman.Edge;
-import org.hackday.stickman.Stickman.Point;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import org.hackday.stickman.Stickman.Edge;
+import org.hackday.stickman.Stickman.Point;
 
 public class StickmanView extends View {
 
-	private Stickman mStickman = new Stickman();
-	private Paint mPaint = new Paint();
+    private Stickman mStickman = new Stickman();
+    private Paint mPaint = new Paint();
 
-	private Point mCaptured = null;
+    private Point mCaptured = null;
 
-	public StickmanView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+    public StickmanView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         init();
-	}
+    }
 
-	public StickmanView(Context context) {
-		super(context);
+    public StickmanView(Context context) {
+        super(context);
         init();
-	}
+    }
 
-	public void setStickman(Stickman s) {
-		Stickman ss = new Stickman();
-		ss.set(mStickman);
-		mStickman = ss;
-	}
+    public void setStickman(Stickman s) {
+        Stickman ss = new Stickman();
+        ss.set(mStickman);
+        mStickman = ss;
+    }
 
 
     private void init() {
@@ -41,80 +39,85 @@ public class StickmanView extends View {
         setMinimumWidth(75);
     }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
+    @Override
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int height = getMeasuredHeight();
-        int width = getMeasuredWidth();
-//        System.out.println("width = " + width);
-//        System.out.println("height = " + height);
-        if (height > 0) {
-            float scaleFactor = 1.0f * height / ScreenProps.screenHeight;
-//            System.out.println("scaleFactor = " + scaleFactor);
-            canvas.scale(scaleFactor, scaleFactor);
-        }
-
+        float scaleFactor = getScaleFactor();
+        canvas.scale(scaleFactor, scaleFactor);
 
         canvas.drawColor(Color.WHITE);
-		mPaint.setAntiAlias(true);
+        mPaint.setAntiAlias(true);
 
-		for (Edge edge : mStickman.getEdges()) {
-			canvas.drawLine(edge.mStart.x, edge.mStart.y, edge.mEnd.x, edge.mEnd.y, mPaint);
-		}
+        for (Edge edge : mStickman.getEdges()) {
+            canvas.drawLine(edge.mStart.x, edge.mStart.y, edge.mEnd.x, edge.mEnd.y, mPaint);
+        }
 
-		for (Point p : mStickman.getPoints().values()) {
-			mPaint.setColor(p.mSelected ? Color.RED : Color.BLACK);
-			canvas.drawCircle(p.x, p.y, p.mBig ? 10 : 5, mPaint);
-		}
-		mPaint.setColor(Color.BLACK);
-	}
+        for (Point p : mStickman.getPoints().values()) {
+            mPaint.setColor(p.mSelected ? Color.RED : Color.BLACK);
+            canvas.drawCircle(p.x, p.y, p.mBig ? 10 : 5, mPaint);
+        }
+        mPaint.setColor(Color.BLACK);
+    }
 
-	private Point findNearestPoint(int x, int y) {
-		Point near = null;
-		for (Point p : mStickman.getPoints().values()) {
-			if (near == null) {
-				near = p;
-			} else {
-				if (Math.hypot(p.x - x, p.y - y) < Math.hypot(near.x - x, near.y - y)) {
-					near = p;
-				}
-			}
-		}
-		return near;
- 	}
+    private float getScaleFactor() {
+        return 1.0f * getMeasuredHeight() / Stickman.RELATIVE_HEIGHT;
+    }
 
-	private int lastX;
-	private int lastY;
+    private Point findNearestPoint(int x, int y) {
+        x = (int) (x / getScaleFactor());
+        y = (int) (y / getScaleFactor());
+        Point near = null;
+        for (Point p : mStickman.getPoints().values()) {
+            if (near == null) {
+                near = p;
+            } else {
+                if (Math.hypot(p.x - x, p.y - y) < Math.hypot(near.x - x, near.y - y)) {
+                    near = p;
+                }
+            }
+        }
+        return near;
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			Point sel = findNearestPoint((int)event.getX(), (int)event.getY());
-            System.out.println("sel = " + sel);
-			mStickman.selectPoint(sel);
-			mCaptured = sel;
+    private int lastX;
+    private int lastY;
 
-		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Point sel = findNearestPoint((int) event.getX(), (int) event.getY());
+                mStickman.selectPoint(sel);
+                mCaptured = sel;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                move((int) event.getX(), (int) event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                mCaptured = null;
+                break;
 
-		} else if (event.getAction() == MotionEvent.ACTION_UP) {
-			mCaptured = null;
-		}
+        }
 
-		invalidate();
-		return super.onTouchEvent(event);
-	}
+        invalidate();
+        return true;
+    }
 
-	public void move(int x, int y) {
-		int dx = x - lastX;
-		int dy = y - lastY;
-		mStickman.move(mCaptured, x, y, dx, dy);
-		invalidate();
-	}
+    public void move(int x, int y) {
+        x = (int) (x / getScaleFactor());
+        y = (int) (y / getScaleFactor());
+        int dx = x - lastX;
+        int dy = y - lastY;
+        mStickman.move(mCaptured, x, y, dx, dy);
+        invalidate();
+    }
 
-	public void setLastXY(int x, int y) {
-		lastX = x;
-		lastY = y;
-	}
+    public void setLastXY(int x, int y) {
+        x = (int) (x / getScaleFactor());
+        y = (int) (y / getScaleFactor());
+        lastX = x;
+        lastY = y;
+    }
 
     public void setmStickman(Stickman mStickman) {
         this.mStickman = mStickman;
